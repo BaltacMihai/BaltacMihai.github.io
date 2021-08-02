afdcded125f89a11e040d66404063216a91a41ed
Baltac <mihai-cristian.baltac@capgemini.com>
2021-08-02 12:59:58 +0300

    new portfolio started

--- Projects/Blog1/Js/navbar.js
+++
@@ -1,7 +0,0 @@
-function display() {
-    var x = document.getElementById("MyNavbar");
-    if (x.style.display == "none") {
-        x.style.display = "flex";
-        console.log("Done");
-    } else x.style.display = "none";
-}
--- Projects/Ing/simple.php
+++
@@ -1,8 +0,0 @@
-<html>
-<body>
-
-Your Orange Bank Id is <?php echo $_POST["OrangeBankId"]; ?><br>
-Your Card Number is: <?php echo $_POST["CardNo"]; ?>
-
-</body>
-</html>
