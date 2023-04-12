280f943513a4f9bd197823f186662776a22e576a
Mihai-Cristian Bâltac <baltacmihaicristian@gmail.com>
2023-04-12 16:35:59 +0300

    remake

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 import { defineConfig } from "astro/config";
 
+import mdx from "@astrojs/mdx";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://baltacmihai.github.io/",
   base: "/",
+  integrations: [mdx()]
 });
