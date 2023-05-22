2e4625c633fedc15f04902aedf04f679e4a1e91f
Mihai-Cristian Bâltac <baltacmihaicristian@gmail.com>
2023-05-22 10:57:03 +0300

    about + some Filter

--- src/components/Filter.vue
+++ src/components/Filter.vue
@@ -43,6 +43,35 @@
       </div>
     </div>
   </div>
+  <div class="content">
+    <template v-for="(card, index) in content" :key="index">
+      <div
+        v-if="
+          (currentContentType.length == 0 && currentSubjects.length == 0) ||
+          (currentContentType.includes(card.contentType) &&
+            currentSubjects.includes(card.subject)) ||
+          (currentContentType.length == 0 &&
+            currentSubjects.includes(card.subject)) ||
+          (currentContentType.includes(card.contentType) &&
+            currentSubjects.length == 0)
+        "
+        class="card"
+      >
+        <img :src="card.image" :alt="card.name" />
+
+        <div class="card-content">
+          <h2>{{ card.name }}</h2>
+          <div class="filterOptions">
+            <p class="option">{{ card.contentType }}</p>
+            <p class="option">{{ card.subject }}</p>
+          </div>
+          <p>{{ card.description }}</p>
+        </div>
+
+        <a :href="'/resources' + card.link" class="btn-secondary">See More</a>
+      </div>
+    </template>
+  </div>
 </template>
 
 <script>
@@ -50,6 +79,7 @@ export default {
   props: {
     contentType: Array,
     subjects: Array,
+    content: Array,
   },
   data() {
     return {
@@ -94,9 +124,6 @@ export default {
         default:
           break;
       }
-
-      console.log(e.target.dataset.title);
-      console.log(e.target);
     },
   },
 };
@@ -108,6 +135,8 @@ export default {
 .filters {
   display: flex;
   gap: 1.5rem;
+  margin-top: 1.5rem;
+  margin-bottom: 1.5rem;
 
   .filter-names {
     display: flex;
@@ -142,6 +171,82 @@ export default {
         background-color: $cl-black;
         color: $cl-white;
       }
+
+      &:hover {
+        cursor: pointer;
+      }
+    }
+  }
+}
+
+.content {
+  display: flex;
+  align-items: stretch;
+  justify-content: space-between;
+  flex-wrap: wrap;
+  gap: 2rem 0;
+  margin-bottom: 2rem;
+
+  .card {
+    flex: 0 0 30%;
+    border: 1px solid #676;
+    border-radius: 10px;
+
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+
+    @include tablet {
+      flex: 0 0 45%;
+    }
+
+    @include mobile {
+      flex: 0 0 100%;
+    }
+
+    .filterOptions {
+      display: flex;
+      align-items: center;
+      gap: 1rem;
+
+      .option {
+        border: 1px solid #676;
+        border-radius: 10px;
+        color: #676;
+        padding: 0.5rem 0.75rem;
+      }
+    }
+
+    img {
+      width: 100%;
+      object-fit: contain;
+      height: 10rem;
+      border-bottom: 1px solid rgba(102, 119, 102, 0.096);
+      padding: 1rem 0;
+    }
+
+    &-content {
+      padding: 1rem;
+      display: flex;
+      flex-direction: column;
+      gap: 1rem;
+      h2 {
+        @extend %paragraf-size;
+      }
+      p {
+        @extend %small-size;
+        color: $cl-grey;
+      }
+    }
+
+    .btn-secondary {
+      width: fit-content;
+      background-color: $cl-black;
+      color: $cl-white;
+      text-decoration: none;
+      margin: 1rem;
+      padding: 0.5rem 0.75rem;
+      border-radius: 10px;
 
       &:hover {
         cursor: pointer;
